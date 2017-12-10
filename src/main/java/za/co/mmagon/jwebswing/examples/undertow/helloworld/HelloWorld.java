818b4208bc5013c365f206f7244cd75a61f51a8e
Marc Magon <ged_marc@hotmail.com>
2017-12-10 04:39:29 +0200

    update dependencies to include undertow

--- src/main/java/za/co/mmagon/jwebswing/examples/undertow/helloworld/HelloWorld.java
+++ src/main/java/za/co/mmagon/jwebswing/examples/undertow/helloworld/HelloWorld.java
@@ -11,35 +11,51 @@ import za.co.mmagon.jwebswing.base.ajax.AjaxResponse;
 import za.co.mmagon.jwebswing.base.angular.AngularPageConfigurator;
 import za.co.mmagon.jwebswing.base.html.Paragraph;
 import za.co.mmagon.jwebswing.plugins.jquery.JQueryPageConfigurator;
+import za.co.mmagon.logger.LogFactory;
 
 import javax.servlet.ServletException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class HelloWorld extends Page
 {
+	private static final Logger log = LogFactory.getLog("UndertowHelloWorld");
+
 	public HelloWorld()
 	{
 		super("Hello World!");
 		add(new Paragraph("Hello World"));
 
-		JQueryPageConfigurator.setRequired(getBody(),true);
-		AngularPageConfigurator.setRequired(getBody(),true);
+		//Enables the onConnect function
+		JQueryPageConfigurator.setRequired(getBody(), true);
+		AngularPageConfigurator.setRequired(getBody(), true);
 	}
 
 	/**
 	 * Not needed at all, but beautiful for post load ajax operations, local storage identifying etc
+	 *
 	 * @param call
+	 * 		A request scoped call object
 	 * @param response
+	 * 		A request scoped response object
+	 *
 	 * @return
 	 */
 	@Override
 	public AjaxResponse onConnect(AjaxCall<?> call, AjaxResponse response)
 	{
+		if (call != null)
+		{
+			log.log(Level.INFO, "This is the call object : {0}", call);
+		}
 		return super.onConnect(call, response);
 	}
 
 	/**
 	 * This part runs the web site :)
+	 *
 	 * @param args
+	 *
 	 * @throws ServletException
 	 */
 	public static void main(String[] args) throws ServletException
