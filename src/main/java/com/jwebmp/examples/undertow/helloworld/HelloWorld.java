5fba9558f2d21e555b664229533f1f3791d8f5fd
Marc Magon <ged_marc@hotmail.com>
2018-04-14 08:26:27 +0200

    Prepping for RC1 - CRP Finalizations Slim Scroll

---
+++ src/main/java/com/jwebmp/examples/undertow/helloworld/HelloWorld.java
@@ -0,0 +1,79 @@
+/*
+ * Copyright (C) 2017 Marc Magon
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.jwebmp.examples.undertow.helloworld;
+
+import com.jwebmp.Page;
+import com.jwebmp.base.html.Paragraph;
+import io.undertow.Undertow;
+import io.undertow.server.HttpHandler;
+import io.undertow.servlet.Servlets;
+import io.undertow.servlet.api.DeploymentInfo;
+import io.undertow.servlet.api.DeploymentManager;
+import za.co.mmagon.guiceinjection.GuiceContext;
+import za.co.mmagon.logger.LogFactory;
+import za.co.mmagon.logger.handlers.ConsoleSTDOutputHandler;
+
+import javax.servlet.ServletException;
+import java.util.logging.Handler;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class HelloWorld
+		extends Page
+{
+	public HelloWorld()
+	{
+		super("Hello World!");
+		add(new Paragraph("Hello World"));
+	}
+
+	/**
+	 * This part runs the web site :)
+	 *
+	 * @param args
+	 *
+	 * @throws ServletException
+	 */
+	public static void main(String[] args) throws ServletException
+	{
+		Handler[] handles = Logger.getLogger("")
+		                          .getHandlers();
+		for (Handler handle : handles)
+		{
+			handle.setLevel(Level.FINE);
+		}
+		LogFactory.setDefaultLevel(Level.FINE);
+		Logger.getLogger("")
+		      .addHandler(new ConsoleSTDOutputHandler(true));
+
+		DeploymentInfo servletBuilder = Servlets.deployment()
+		                                        .setClassLoader(HelloWorld.class.getClassLoader())
+		                                        .setContextPath("/")
+		                                        .setDeploymentName("helloworld.war");
+		DeploymentManager manager = Servlets.defaultContainer()
+		                                    .addDeployment(servletBuilder);
+		manager.deploy();
+		GuiceContext.inject();
+		HttpHandler jwebSwingHandler = manager.start();
+		Undertow server = Undertow.builder()
+		                          .addHttpListener(6002, "localhost")
+		                          .setHandler(jwebSwingHandler)
+		                          .build();
+		server.start();
+	}
+}
