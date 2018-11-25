39172f74e56806c21bb62ad1dc4d39c20739c3f7
GedMarc <ged_marc@hotmail.com>
2018-11-25 21:28:02 +0200

    Updates for Jackson

---
+++ src/jre11/java/module-info.java
@@ -0,0 +1,23 @@
+import com.jwebmp.core.services.IPage;
+import com.jwebmp.examples.undertow.helloworld.HelloWorld;
+import com.jwebmp.examples.undertow.helloworld.HelloWorldCSS;
+import com.jwebmp.examples.undertow.helloworld.HelloWorldCSSObject;
+import com.jwebmp.examples.undertow.helloworld.HelloWorldPlain;
+
+module com.jwebmp.examples.undertow.helloworld {
+
+	exports com.jwebmp.examples.undertow.helloworld to com.google.guice;
+
+	requires javax.servlet.api;
+	requires java.logging;
+
+	requires undertow.core;
+	requires undertow.servlet;
+
+	requires com.jwebmp.core;
+	requires com.jwebmp.undertow;
+
+	provides IPage with HelloWorld, HelloWorldCSS, HelloWorldCSSObject, HelloWorldPlain;
+
+	opens com.jwebmp.examples.undertow.helloworld to com.google.guice;
+}
